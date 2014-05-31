87f0c230ffedd50f40d6772fe1ba5fdba762ba10
Dylan Richards <drichards2013@gmail.com>
2014-05-31 07:49:03 -0400

    fixed routes for blog

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,18 @@ Dylanrichards::Application.routes.draw do
   get 'pages/contact'
   get 'pages/programming'
 
+
+  get 'photography' => 'pages#photography', as: 'photography'
+  get 'about' => 'pages#about', as: 'about'
+  get 'projects' => 'pages#projects', as: 'projects'
+  get 'blog/running' => 'pages#running', as: 'running'
+  get 'blog/mindfulness' => 'pages#mindfulness', as: 'mindfulness'
+  get 'blog/meditation' => 'pages#meditation', as: 'meditation'
+  get 'blog/programming' => 'pages#programming', as: 'programming'
+  get 'contact' => 'pages#contact', as: 'contact'
+
+
+
   get 'tags/:tag', to: 'posts#index', as: :tag
 
 
