8eeccb5cd37b1011415f0ddce5717ea516d9f319
Dylan Richards <drichards2013@gmail.com>
2014-07-13 01:46:50 -0400

    removed fucking photoGallery lightbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,29 +26,7 @@ $(document).ready(function(){
 
   $('.fadein').addClass('animated fadeInDown');
   $('.pagetitle').addClass('animated bounceInDown');
-
-  var $overlay = $('<div id="overlay"></div>');
-  var $image = $('<img>')
-
-  $overlay.append($image);
-  $('body').append($overlay);
-
-  $('#imageGallery a').click(function(event) {
-  	event.preventDefault();
-  	var imageLocation = $(this).attr("href");
-
-  	$image.attr('src', imageLocation);
-  	$overlay.show();
-  });
-
-  $overlay.click(function(event) {
-  	event.preventDefault();
-  	$(this).hide();
-  });
-
-
-
-});
+})
 
 
 
