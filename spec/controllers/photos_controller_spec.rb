fde045f28f35392b10d59f1ceda257a2a5a2660d
Dylan Richards <drichards2013@gmail.com>
2014-08-30 21:53:02 -0400

    specs for posts controller

--- spec/controllers/photos_controller_spec.rb
+++ spec/controllers/photos_controller_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 describe PhotosController do
-  
+
+  let(:photo) { FactoryGirl.create(:photo) }
+
   describe '#index' do
     it 'renders the index template' do
       get :index
@@ -14,4 +16,35 @@ describe PhotosController do
     end
   end
 
+  describe '#show' do
+    it 'renders the show template' do
+      get :show, id: photo
+      expect(response).to render_template :show
+    end
+
+    it 'assigns @photo to the correct photo' do
+      get :show, id: photo
+      expect(assigns(:photo)).to eq photo
+    end
+  end
+
+  describe '#create' do
+    it 'saves the new post in the database' do
+      expect {
+        FactoryGirl.create(:post)
+      }.to change(Post, :count).by 1
+    end
+  end
+
+  describe '#edit' do
+    it 'renders the edit template' do
+      get :edit, id: photo
+      expect(response).to render_template :edit
+    end
+
+    it 'assigns the correct photo to @photo' do
+      get :edit, id: photo
+      expect(assigns(:photo)).to eq photo
+    end
+  end
 end
