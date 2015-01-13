9f965c9e90bc35d2ae8960ac1c64e56cee0bcb36
Dylan Richards <dylanerichards@gmail.com>
2015-01-12 19:21:09 -0500

    Valite presence of image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,4 +3,6 @@ class Photo < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "240x240>", :thumb => "75x75>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+
+  validates :image, presence: true
 end
