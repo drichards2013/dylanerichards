4edb9345824ecf28e7046fe9017dd21d1ed79583
Dylan Richards <dylanerichards@gmail.com>
2015-01-26 11:14:53 -0500

    Don't scale image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,7 +1,7 @@
 class Photo < ActiveRecord::Base
   default_scope order: 'created_at DESC'
 
-  has_attached_file :image, :styles => { :medium => "240x240>", :thumb => "75x75>" }
+  has_attached_file :image, :styles => { :medium => "240x240", :thumb => "75x75>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   validates :image, presence: true
