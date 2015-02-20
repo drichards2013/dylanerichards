e8dc9e23789600467c404f189260e9be6fc704d6
Dylan Richards <dylanerichards@gmail.com>
2015-02-20 17:46:42 -0500

    Test Post#to_param

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -3,4 +3,12 @@ require 'rails_helper'
 describe Post do
   it { should validate_presence_of (:title) }
   it { should validate_presence_of (:body) }
+
+  describe "#to_param" do
+    post = Post.create(title: "Hello", body: "World")
+
+    it "parameterizes based on id and title" do
+      expect(post.to_param).to eq "#{post.id}-#{post.title.downcase}"
+    end
+  end
 end
