614104b109ce95ea871819b65bf46da1d4787188
Dylan Richards <dylanerichards@gmail.com>
2015-04-19 16:21:01 -0400

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,22 +9,22 @@ Dylanrichards::Application.routes.draw do
   get '/reading-list' => 'reading_lists#show', id: 1, as: 'main_reading_list'
   get '/reading-list/edit' => 'reading_lists#edit', id: 1
 
-  devise_for :users do
-    get '/users/sign_out' => 'devise/sessions#destroy'
-  end
-
   get 'blog' => 'posts#index', as: 'blog'
 
   get 'projects' => 'pages#projects', as: 'projects'
   get 'photography' => 'photos#index', as: 'photogallery'
 
+  get 'tags/:tag', to: 'posts#index', as: :tag
+
   %w(contact about).each do |page|
     get "#{page}" => "pages##{page}", as: "#{page}"
   end
 
-  %w(running mindfulness meditation photography programming contact about).each do |tag|
+  %w(running mindfulness meditation photography programming).each do |tag|
     get "tags/#{tag}" => "pages##{tag}", as: "#{tag}"
   end
 
-  get 'tags/:tag', to: 'posts#index', as: :tag
+  devise_for :users do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
