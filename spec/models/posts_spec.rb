5934cde4dfdb86ba17fb7bcd43a02296163851ee
Dylan Richards <dylanerichards@gmail.com>
2015-12-14 22:00:12 -0500

    Refactor post creation to let clause

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
 
 describe Post do
+  let(:post) { Post.create(title: "Hello", body: "World") }
+
   it { should validate_presence_of (:title) }
   it { should validate_presence_of (:body) }
 
   describe "#to_param" do
-    post = Post.create.tap do |p|
-      p.title = "Hello"
-      p.body = "World"
-      p.save
-    end
-
     it "parameterizes based on id and title" do
       expect(post.to_param).to eq "#{post.id}-#{post.title.downcase}"
     end
