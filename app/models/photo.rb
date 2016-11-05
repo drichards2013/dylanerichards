6ff9642ff94f27f45194c93c38da92003e310a83
Dylan Richards <dylanerichards@gmail.com>
2016-11-04 19:35:19 -0700

    Add xmedium size

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,12 @@
 class Photo < ActiveRecord::Base
   default_scope order: 'created_at DESC'
 
-  has_attached_file :image, :styles => { large: "600x600>", medium: "400x400>", small: "200x200>" }
+  has_attached_file :image, :styles => { xmedium: "500x500>", large: "600x600>", medium: "400x400>", small: "200x200>" }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
   validates :image, presence: true
 
   def random_size
-    [:small, :medium, :large].sample
+    [:xmedium, :medium, :large].sample
   end
 end
