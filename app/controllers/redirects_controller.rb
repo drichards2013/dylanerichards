ea0fd211cd0dd6b64d9ddc3cb1ef0f3c00e4961d
Dylan Richards <dylanerichards@gmail.com>
2017-05-02 00:30:55 -0400

    Redirect the redirects.

--- app/controllers/redirects_controller.rb
+++ app/controllers/redirects_controller.rb
@@ -1,17 +1,17 @@
 class RedirectsController < ApplicationController
   def escalate
-    redirect_to "https://www.escalatebook.com"
+    redirect_to "https://www.escalate-photography.com"
   end
 
   def masterclass
-    redirect_to "https://www.escalatebook.com/masterclass"
+    redirect_to "https://www.escalate-photography.com/masterclass"
   end
 
   def vsco
-    redirect_to "https://www.escalatebook.com/vsco"
+    redirect_to "https://www.escalate-photography.com/vsco"
   end
 
   def vip
-    redirect_to "https://www.escalatebook.com/vip"
+    redirect_to "https://www.escalate-photography.com/vip"
   end
 end
